3fd7a3cfe822b6994002c1e106a11bb465aefad8
Eftimin70 <106393682+Eftimin70@users.noreply.github.com>
2026-08-10 15:33:29 +0200

    Update amf_d3d11.cpp

--- src/amf/amf_d3d11.cpp
+++ src/amf/amf_d3d11.cpp
@@ -1000,7 +1000,7 @@ namespace amf {
     int64_t ctu_size = 64;
     int64_t ctu_width = (encode_width + (ctu_size - 1)) / ctu_size;
     int64_t ctu_height = (encode_height + (ctu_size - 1)) / ctu_size;
-    int64_t total_ctus = ctu_width * ctu_height;
+//    int64_t total_ctus = ctu_width * ctu_height;
 
     int64_t ctu_rows_per_frame = (ctu_height + gop_size - 1) / gop_size;
     if (ctu_rows_per_frame < 1) ctu_rows_per_frame = 1; // Mindestens eine Zeile pro Frame
