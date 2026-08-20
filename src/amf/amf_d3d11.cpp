4c8dcec6193a0fe33f074b8bde1fd97e05101f83
Eftimin70 <106393682+Eftimin70@users.noreply.github.com>
2026-08-20 12:32:00 +0200

    Update amf_d3d11.cpp
    added INSERT2 part again

--- src/amf/amf_d3d11.cpp
+++ src/amf/amf_d3d11.cpp
@@ -1898,7 +1898,30 @@ if (client_config.enableIntraRefresh == 1) {
       disable_rfi_after_property_failure(label, property_result);
       return false;
     };
- 
+
+// BEGIN INSERT2
+// Check if client requested GDR
+if (encoder && client_config.enableIntraRefresh == 1) {
+    AMF_RESULT res;
+    if (force_idr) {
+        res = surface->SetProperty(AMF_VIDEO_ENCODER_HEVC_FORCE_PICTURE_TYPE, AMF_VIDEO_ENCODER_HEVC_PICTURE_TYPE_NONE);
+        if (res != AMF_OK) {
+            BOOST_LOG(warning) << "AMF GDR: Failed to set FORCE_PICTURE_TYPE (IDR interception)";
+        }
+        res = surface->SetProperty(AMF_VIDEO_ENCODER_HEVC_INSERT_HEADER, true);
+        if (res != AMF_OK) {
+            BOOST_LOG(warning) << "AMF GDR: Failed to set INSERT_HEADER";
+        }
+    } else {
+        // During regular frames, leave picture type to NONE so the driver decides the pacing autonomously
+        res = surface->SetProperty(AMF_VIDEO_ENCODER_HEVC_FORCE_PICTURE_TYPE, AMF_VIDEO_ENCODER_HEVC_PICTURE_TYPE_NONE);
+        if (res != AMF_OK) {
+            BOOST_LOG(warning) << "AMF GDR: Failed to set FORCE_PICTURE_TYPE (Normal operation)";
+        }
+    }
+}
+// END INSERT2
+    
     auto set_forced_idr_properties = [&]() {
       auto check = [&](AMF_RESULT property_result, const char *label) {
         if (property_result == AMF_OK) return true;
